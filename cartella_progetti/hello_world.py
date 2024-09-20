0bc6d26fca37619d36ce2864fd9063b670941455
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-09-20 12:49:10 +0000

    CI: Implementing jsonschema

---
+++ cartella_progetti/hello_world.py
@@ -0,0 +1,14 @@
+print('Hello World!')
+
+
+from jsonschema import validate
+
+schema = {
+    "type" : "object",
+    "properties" : {
+        "price" : {"type" : "number"},
+        "name" : {"type" : "string"},
+    },
+}
+
+validate(instance={"name" : "Eggs", "price" : 34.99}, schema=schema)

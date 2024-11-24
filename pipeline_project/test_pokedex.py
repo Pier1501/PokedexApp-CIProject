5ce7baa0f9554169a17e6dd8450b6dbe791b5d09
Pier1501 <96800487+Pier1501@users.noreply.github.com>
2024-11-24 16:24:03 +0000

    Added pytest for get type function

--- pipeline_project/test_pokedex.py
+++ pipeline_project/test_pokedex.py
@@ -1,5 +1,6 @@
 from jsonschema import validate
 import json
+import pytest
 
 from pokedex import get_pokemon_data, get_pokemon_types
 from pokedex import calculate_type_effectiveness
@@ -36,3 +37,42 @@ def validate_wrapper(schema, pokemon_name):
     except Exception as e:
         print(f"Schema validation error: {e}")
         return False
+    
+
+def test_get_pokemon_types_success():
+    charizard_data = {
+        "types": [
+            {"type": {"name": "FIRE"}},
+            {"type": {"name": "FLYING"}}
+        ]
+    }
+    assert get_pokemon_types(charizard_data) == ["fire", "flying"]
+    
+    gyarados_data = {
+        "types": [
+            {"type": {"name": "WATER"}},
+            {"type": {"name": "FLYING"}}
+        ]
+    }
+    assert get_pokemon_types(gyarados_data) == ["water", "flying"]
+
+def test_get_pokemon_types_error(pokemon_name = 'Torchic'):
+    test_data = get_pokemon_data(pokemon_name)
+    test_types = get_pokemon_types(test_data)
+
+    assert "fire" in test_types, f"Expected {pokemon_name} to be Fire type, but got {test_types}"
+
+    torchic_wrong_data = {
+        "types": [
+            {"type": {"name": "GRASS"}}
+        ]
+    }
+    with pytest.raises(KeyError):
+        get_pokemon_types(torchic_wrong_data)
+
+    with pytest.raises(TypeError):
+        get_pokemon_types(None)
+
+
+
+
